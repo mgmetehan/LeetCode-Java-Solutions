1c494f980b5a2c11c78c36c9b1eeeef1a67f3138
mgmetehan <mgmetehan@hotmail.com>
2023-03-30 16:44:04 +0300

    🎉 feat(BestTimetoBuyandSellStock2_Medium.java): add solution for Best Time to Buy and Sell Stock II problem This commit adds a solution for the Best Time to Buy and Sell Stock II problem in LeetCode. The solution uses a greedy approach to find the maximum profit that can be obtained by buying and selling stocks. The solution is implemented in the maxProfit method, which takes an array of stock prices as input and returns the maximum profit that can be obtained. The main method is used to test the solution with a sample input.

---
+++ LeetCode/src/BestTimetoBuyandSellStock2_Medium.java
@@ -0,0 +1,22 @@
+public class BestTimetoBuyandSellStock2_Medium {
+    public static int maxProfit(int[] prices) {
+        if (prices.length == 0 || prices == null) {
+            return 0;
+        }
+
+        var result = 0;
+
+        for (var i=0; i<prices.length-1; i++) {
+            if (prices[i] < prices[i+1]) {
+                result += prices[i+1] - prices[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 4, 5};
+        var result = new BestTimetoBuyandSellStock2_Medium().maxProfit(prices);
+        System.out.println(result);
+    }
+}
