c325b2bcd9e60ea05c99264291a52a9494d541c6
mgmetehan <mgmetehan@hotmail.com>
2023-03-30 15:16:02 +0300

    🎉 feat(AddTwoNumbers_2_Medium.java): add solution for adding two numbers as linked lists This commit adds a solution for the LeetCode problem #2 - Add Two Numbers. The solution takes two linked lists as input, where each node in the linked list represents a digit of a number in reverse order. The solution adds the two numbers and returns the sum as a linked list in reverse order. The solution uses a temporary node to keep track of the sum and an extra variable to keep track of the carry. The solution iterates over the linked lists and adds the digits, taking into account the carry. Finally, the solution returns the sum as a linked list. The commit also includes a main method to test the solution.

--- LeetCode/src/AddTwoNumbers_2_Medium.java
+++ LeetCode/src/AddTwoNumbers_2_Medium.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class AddTwoNumbers_2_Medium {
+public class AddTwoNumbers_2_Medium {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        var temp = new ListNode(0);
+        var l3 = temp;
+
+        var extra = 0;
+        while (l1 != null || l2 != null) {
+            var l1Value = (l1 != null) ? l1.val : 0;
+            var l2Value = (l2 != null) ? l2.val : 0;
+
+            var sum = l1Value + l2Value + extra;
+            extra = sum / 10;
+            var lastDigit = sum % 10;
+
+            var newNode = new ListNode(lastDigit);
+            l3.next = newNode;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        if (extra > 0) {
+            var newNode = new ListNode(extra);
+            l3.next = newNode;
+            l3 = l3.next;
+        }
+        return temp.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(1)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode l3 = addTwoNumbers(l1, l2);
+        while (l3 != null) {
+            System.out.print(l3.val);
+            l3 = l3.next;
+        }
+    }
 }
